cd5dd7cc8759b1aba446004ea9ab16dc142fb2d8
Rik Meijer <hello@rikmeijer.nl>
2024-03-27 11:16:30 +0100

    Update cron.php

--- cron.php
+++ cron.php
@@ -4,7 +4,11 @@ use Masterminds\HTML5;
 
 function myip() : string {
     $ipElement = new DOMXPath((new HTML5())->loadHTMLFile('https://www.whatismyip.org/'));
-    $ipText = $ipElement->query('//*[contains(text(), "Your IP:")]')->item(0)->parentNode->textContent;
+    $response = $ipElement->query('//*[contains(text(), "Your IP:")]');
+    if ($response->count() === 0) {
+        exit('can not determine ip');
+    }
+    $ipText = $response->item(0)->parentNode->textContent;
     if (preg_match('/((\d+\.){3}(\d+))/', $ipText, $matches) === false) {
         exit('can not determine ip');
     }
